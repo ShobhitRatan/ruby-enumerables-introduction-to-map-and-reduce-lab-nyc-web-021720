49244b6810814cc73accf8715b8ed32b08add7d4
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-06 01:56:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,5 +24,5 @@ end
 
 #Mthod to return a running total when not given a starting point.
 def reduce_to_total(source_array, starting_point = 0)
-  return source_array.reduce() {|sum, n| sum + n}
+  return source_array.reduce() {|sum,n| sum + n}
 end
