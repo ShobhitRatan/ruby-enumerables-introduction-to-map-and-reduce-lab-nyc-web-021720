978d7a1ba96c6c8d853ca85e3a1ebf01de9dbe24
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-06 01:49:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,11 @@
 # My Code here....
+# Method to negatie an array.
 def map_to_negativize(array)
   n_a = array.map{|n| n*-1} #new array
   return n_a
-end 
+end
+
+# Method to return an array with the original values.
+def map_to_no_change(array)
+  return array
+end
